9c0778a7eaabe8be0587606f836f88ed6c48bc19
IreneMercy <irenemercy700@gmail.com>
2018-05-20 00:55:03 -0700

    add,calculations

---
+++ test.py
@@ -0,0 +1,15 @@
+ num1 = 12
+ num2 = 14
+ num3 = 5
+
+ calc = {}
+ sum = num1 + num2
+ product = num2 * num3
+
+ subtraction = num1 - num3
+ 
+
+ calc["product"] = product
+
+ calc["subtraction"] = subtraction
+
